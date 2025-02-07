c4d4438cb1b8083e23528bdecd57b6b7ae108848
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-02-07 10:45:36 +0100

    class konyvek megalkotva

---
+++ gitOsztaly/Konyv.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitOsztaly
+{
+	internal class Konyv
+	{
+		private string cim;
+		private string szerzo;
+		private int kiadasEve;
+		private int oldalszam;
+		private int ar;
+		
+		// konyv.Cim - A saját könyv.Cia = "A sajt"
+		// konyv.Cim = "Cim"
+		public string Cim { get => cim; set => cim = value; }
+		public string Szerzo { get => szerzo; set => szerzo = value; }
+		public int KiadasEve { get => kiadasEve; set => kiadasEve = value; }
+		public int Oldalszam { get => oldalszam; set => oldalszam = value; }
+		public int Ar { get => ar; set => ar = value; }
+
+		public Konyv(string cim, string szerzo, int kiadasEve, int oldalszam, int ar)
+		{
+			this.cim = cim;
+			this.szerzo = szerzo;
+			this.kiadasEve = kiadasEve;
+			this.oldalszam = oldalszam;
+			this.ar = ar;
+		}
+			
+		public Konyv(string cim, string szerzo)
+		{
+			this.cim = cim;
+			this.szerzo = szerzo;
+			kiadasEve = DateTime.Now.Year;
+			ar = 3000;
+		}
+	}
+
+}
