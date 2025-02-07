72d5b0e2aa42436fdeab26312beff0cd2dce160a
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-02-07 11:15:41 +0100

    Konyv konstruktorok létrehozva

--- gitOsztaly/Program.cs
+++ gitOsztaly/Program.cs
@@ -10,6 +10,15 @@ namespace gitOsztaly
 	{
 		static void Main(string[] args)
 		{
-		}
+			Konyv konyv1 = new("A Kis Herceg", "Antoine de Saint-Exupéry", 1943, 96, 2500);
+			Konyv konyv2 = new("Ez még készül", "Én");
+
+            Console.WriteLine(konyv1.ToString() + "\n" + konyv2.ToString());
+
+			konyv1.Kezdvezmeny(20);
+			konyv1.Arnoveles(1000);
+
+            Console.WriteLine("\n" + konyv1.ToString() + "\n" + konyv1.HanyOldalMaradt(20));
+        }
 	}
 }
